d4177464095bd5d9d15917f20e945be339d40ff9
Jesus Arturo Carmona Aguirre <jessarturo97@gmail.com>
2022-10-18 17:47:31 -0500

    removal of hero and header folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import './App.css';
-// import Header from './components/header/Header';
-import Hero from './components/hero/Hero';
 import Features from './components/features/Features';
 import Hotels from './components/hotels/Hotels';
 
@@ -8,10 +6,8 @@ import Hotels from './components/hotels/Hotels';
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
 
       <main>
-        <Hero />
         <Features />
         <Hotels />
       </main>
